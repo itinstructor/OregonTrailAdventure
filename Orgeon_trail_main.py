d09e7d2ce55bec271a6766e40e5cbac4bfeda2c8
Braxton Rider <rider.braxton@gmail.com>
2023-10-12 11:36:01 -0600

    Added skeleton to main

---
+++ Orgeon_trail_main.py
@@ -0,0 +1,37 @@
+"""
+Name:Orgeon_trail_main
+
+"""
+
+#Import other files
+
+
+#Define main
+def main():
+    #Print creative title
+    print("Creative title")
+
+    #Set up loop
+    still_running = "y"
+    while still_running == "y":
+
+        #Arturo's stop
+        #Check if they passed the room
+
+        #Aiden's stop
+        #Check if they passed the room
+
+        #Gus' stop
+        #Check if they passed the room
+
+
+        #Josiah's stop
+        #Check if they passed the room
+
+
+        #Yonaton's stop
+        #Check if they passed the room
+
+    #Decide if user wants to play again
+        pass
+    pass
