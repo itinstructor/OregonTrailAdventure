7b606aee165d1212df0dec1f2ab4930bb8a12f0d
Braxton Rider <rider.braxton@gmail.com>
2023-10-12 14:27:18 -0600

    Added main skeleton
    Added most of the skeleton of the game. For the most part, we just need to add each individual room. For now, there are just methods within the Game() class, but will later be from separate files

--- Oregon_trail_main.py
+++ Oregon_trail_main.py
@@ -4,34 +4,227 @@ Name:Orgeon_trail_main
 """
 
 #Import other files
-
+#This will be were other members import their files for their rooms
 
 #Define main
 def main():
-    #Print creative title
-    print("Creative title")
+
+    #Print creative title (This will only print once, so it is before the loop)
+    print("\nCreative title")
+    print()
 
     #Set up loop
+    #This loop is for the entire main program from start to finish. After that, we ask the user if they want to start over
     still_running = "y"
     while still_running == "y":
+        
+        #Main part of program
+        main_game()
+
+        #Ask user if they would like to play again
+        still_running = input("Would you like to play again? [Y/N]: ").lower()
+
+    #If user exits
+    print("Thank you for playing!")
+
+#This is the program that runs the game
+def main_game():
+    
+    #The loop will always run up unless player health were to ever hit zero, then the loop breaks
+    while True:
+
+
+
+        #Create object
+        player = Game()
+
+
+
+
 
         #Arturo's stop
-        #Check if they passed the room
+        player.health_lost = player.arturo_stop()
+
+        #Subtract health from the amount of health lost during room
+        health = player.subract_health()
+
+        #Check health and end game if the player has no health
+        check = player.check_health()
+
+        if check == 0:
+            player.game_over()
+            break
+
+        
 
         #Aiden's stop
-        #Check if they passed the room
+        player.health_lost =  player.aiden_stop()
+
+        #Subtract health from the amount of health lost during room
+        player.subract_health()
+
+        #Check health and end game if the player has no health
+        check = player.check_health()
+
+        if check == 0:
+            player.game_over()
+            break
+
 
         #Gus' stop
-        #Check if they passed the room
+        player.health_lost = player.gus_stop()
 
+        #Subtract health from the amount of health lost during room
+        player.subract_health()
+
+        #Check health and end game if the player has no health
+        check = player.check_health()
+
+        if check == 0:
+            player.game_over()
+            break
+        
 
         #Josiah's stop
-        #Check if they passed the room
+        player.health_lost = player.josiah_stop()
+
+        #Subtract health from the amount of health lost during room
+        player.subract_health()
+
+        #Check health and end game if the player has no health
+        check = player.check_health()
+
+        if check == 0:
+            player.game_over()
+            break
 
 
         #Yonaton's stop
-        #Check if they passed the room
+        player.health_lost = player.yonaton_stop()
 
-    #Decide if user wants to play again
+        #Subtract health from the amount of health lost during room
+        player.subract_health()
+
+        #Check health and end game if the player has no health
+        check = player.check_health()
+
+        if check == 0:
+            player.game_over()
+            break
+
+
+
+        #If user reaches end, congrats
+        print("You beat the game!")
+
+        #End program funciton
+        break
+
+
+    
+
+        
+#Make object
+class Game():
+
+    #Initialize game health. This will be 100 at the beginning and will be lowered as player goes through rooms
+    def __init__(self):
+
+
+        self._health = 100
+        self._health_lost = 0
+
+    #If player health hits zero. We can later add cause of death later
+    def game_over(self):
+        print("You died!")
+
+    #-------------------------------------------------ROOMS----------------------------------------------------
+    #Each member will design their own rooms with their own challanges and what not. As of the current moment,
+    #The only thing that will be returned is the amount of health lost during that room. Once the player goes
+    #Through the room (Goes through the method), the program will check if the user's health is above 0 or not
+    #If user health is not greater than 0, program ends.
+
+    #So the
+    #---------------------------------------------------------------------------------------------------------
+    #Stop 1
+    def arturo_stop(self):
         pass
-    pass
+        #Return the total amount of health lost during time in room
+
+
+
+
+
+    #Stop 2
+    def aiden_stop(self):
+        pass
+        #Return the total amount of health lost during time in room
+
+
+
+
+
+
+    #Stop 3
+    def gus_stop(self):
+        pass
+        #Return the total amount of health lost during time in room
+
+
+
+
+
+
+    #Stop 4
+    def josiah_stop(self):
+        pass
+        #Return the total amount of health lost during time in room
+
+
+
+
+
+
+    #Stop 5 (End of game)
+    def yonaton_stop(self):
+        pass
+        #Return the total amount of health lost during time in room
+
+
+
+
+
+
+
+
+    #-----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+    #---------------------------------------------------CALCULATIONS--------------------------------------------------
+    #These will end up going into a different file, since this file will just be for displaying.
+    #Calculations like subtracting and checking health
+    #-----------------------------------------------------------------------------------------------------------------
+    def subract_health(self,):
+        self._health = self._health - self._health_lost
+
+        return self._health
+    #Check current health
+    def check_health(self):
+        
+        #If health is less than or equal to zero, end game
+        if self._health <= 0:
+            return 0
+            
+        #If health is greater than zero, print health and proceed
+        print(f"Your health is at {self._health}")
+
+
+
+
+
+#If alone, else module
+if __name__ == "__main__":
+    main()
